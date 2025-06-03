1b681ead92c5675875ab721db030d558e527e08d
Jujun8 <juanabanat2@gmail.com>
2025-06-03 20:05:00 +0700

    Update untitled7 (2).py

--- untitled7 (2).py
+++ untitled7 (2).py
@@ -22,7 +22,7 @@ df.head(16)
 
 import pandas as pd
 
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 
