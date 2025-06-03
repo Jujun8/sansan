55c0889af60b08e726932bc1df05e41939499534
Jujun8 <juanabanat2@gmail.com>
2025-06-03 20:05:58 +0700

    Update untitled7 (2).py

--- untitled7 (2).py
+++ untitled7 (2).py
@@ -55,7 +55,7 @@ X = df[text_column]
 y = df[target_column]
 
 # Split data training dan testing
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 
 # TF-IDF Vectorizer
 # Gunakan stop_words='english' atau stop_words=None jika data non-Inggris dan tidak ada stop word list khusus
