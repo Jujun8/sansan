e1d0889a5f84a0ca1dd5b7b7b0ead1b1625d0ce0
Jujun8 <juanabanat2@gmail.com>
2025-06-03 20:04:35 +0700

    Update untitled7 (2).py

--- untitled7 (2).py
+++ untitled7 (2).py
@@ -21,7 +21,7 @@ df.head(16)
 # !pip install scikit-learn pandas
 
 import pandas as pd
-from sklearn.model_selection import train_test_split
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
