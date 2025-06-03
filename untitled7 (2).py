2f38fe52e578d84600137f26262758d21250032d
Jujun8 <juanabanat2@gmail.com>
2025-06-03 20:23:48 +0700

    Update untitled7 (2).py

--- untitled7 (2).py
+++ untitled7 (2).py
@@ -10,6 +10,8 @@ from sklearn.metrics import classification_report
 # Load dataset
 url = "https://raw.githubusercontent.com/Jujun8/sansan/main/data%20proyek.csv"
 df = pd.read_csv(url)
+print(df.columns.tolist())
+
 
 # Cek nama kolom
 print("Kolom tersedia:", df.columns)
